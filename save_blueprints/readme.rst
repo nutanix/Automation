9917f408e41eb58b85f8b12c88533866e419a798
Chris Rasmussen <chris@digitalformula.net>
2018-09-21 14:02:51 +1000

    Add save_blueprints.py utility script

---
+++ save_blueprints/readme.rst
@@ -0,0 +1,60 @@
+Save Calm Blueprints
+====================
+
+Python 3.6 script to connect to Prism Central 5.9 or above and export all blueprints to JSON files.
+
+Disclaimer
+----------
+
+This is *not* a production-grade script.  Please make sure you add appropriate exception handling and error-checking before running it in production.  See note re versions below, too.
+
+Author
+------
+
+Chris Rasmussen, Solutions Architect, Nutanix (Melbourne, AU)
+
+Changelog
+---------
+
+- 2018.09.21 - Script created
+
+Details
+-------
+
+Connect to a Nutanix Prism Central instance, grab all Calm blueprints and save them to JSON files.
+
+The intention is to use this script to take a quick backup of your Calm blueprints, without the need to manually export each one individually.
+
+The other idea is for you, the user, to take this script and modify it to suit your requirements.
+
+Requirements
+------------
+
+- Python3.6
+- pip3 (to install requirements below, if you need to)
+- requests
+- urllib3
+- pipenv (optional, but recommended)
+
+Script Usage (Linux)
+----------------------
+
+::
+
+    ./src/save_blueprints.py <prism_central_ip_address> [ --username username ] [ --password password ]
+
+Screenshot
+----------
+
+..
+
+    image:: images/screenshot.png
+
+Support
+-------
+
+These scripts are *unofficial* and are not supported or maintained by Nutanix in any way.
+
+In addition, please also be advised that these scripts may run and operate in a way that do not follow best practices.  Please check through each script to ensure it meets your requirements.
+
+**Changes will be required before these scripts can be used in production environments.**
---
+++ save_blueprints/src/apiclient/__init__.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3.6
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+class ApiClient():
+
+    def __init__(self, method, cluster_ip, request, body, username, password):
+        self.method = method
+        self.cluster_ip = cluster_ip
+        self.username = username
+        self.password = password
+        self.base_url = f"https://{self.cluster_ip}:9440/api/nutanix/v3"
+        self.entity_type = request
+        self.request_url = f"{self.base_url}/{request}"
+        self.body = body
+
+    def get_info(self, show_info=False):
+
+        if show_info == True:
+            print(f"Requesting '{self.entity_type}' ...")
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        try:
+            if(self.method == 'post'):
+                r = requests.post(self.request_url, data=self.body, verify=False, headers=headers, auth=HTTPBasicAuth(self.username, self.password), timeout=5)
+            else:
+                r = requests.get(self.request_url, verify=False, headers=headers, auth=HTTPBasicAuth(self.username, self.password), timeout=5)
+        except requests.ConnectTimeout:
+            print(f'Connection timed out while connecting to {self.cluster_ip}. Please check your connection, then try again.')
+            sys.exit()
+        except requests.ConnectionError:
+            print(f'An error occurred while connecting to {self.cluster_ip}. Please check your connection, then try again.')
+            sys.exit()
+        except requests.HTTPError:
+            print(f'An HTTP error occurred while connecting to {self.cluster_ip}. Please check your connection, then try again.')
+            sys.exit()
+        
+        if r.status_code >= 500:
+            print(f'An HTTP server error has occurred ({r.status_code})' ) 
+        else:
+            if r.status_code == 401:
+                print(f'An authentication error occurred while connecting to {self.cluster_ip}. Please check your credentials, then try again.')
+                sys.exit()
+            if r.status_code >= 401:
+                print(f'An HTTP client error has occurred ({r.status_code})' )
+                sys.exit()
+            # else:
+                # print('Connected and authenticated successfully.')
+        
+        return(r.json())
